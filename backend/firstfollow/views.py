031154f139af23ae4b59499664439c40fb683775
unknown <akritiuniyal31@gmail.com>
2025-05-19 00:18:39 +0530

    firstfollow

--- backend/firstfollow/views.py
+++ backend/firstfollow/views.py
@@ -1,127 +1,144 @@
-from django.http import JsonResponse
+from re import *
 from collections import OrderedDict
-import json
+
+t_list=OrderedDict()
+nt_list=OrderedDict()
+production_list=[]
+
+
 
 class Terminal:
+
     def __init__(self, symbol):
-        self.symbol = symbol
+        self.symbol=symbol
 
     def __str__(self):
         return self.symbol
+
+
 
 class NonTerminal:
+
     def __init__(self, symbol):
-        self.symbol = symbol
-        self.first = set()
-        self.follow = set()
+        self.symbol=symbol
+        self.first=set()
+        self.follow=set()
 
     def __str__(self):
         return self.symbol
 
-    def add_first(self, symbols):
-        self.first |= set(symbols)
+    def add_first(self, symbols): self.first |= set(symbols) 
 
-    def add_follow(self, symbols):
-        self.follow |= set(symbols)
+    def add_follow(self, symbols): self.follow |= set(symbols)
 
-def firstfollow(request):
-    production_list = []
-    t_list = OrderedDict()
-    nt_list = OrderedDict()
 
-    if request.method == "POST":
-        try:
-            if request.content_type == 'application/json':
-                body = json.loads(request.body)
-                data = body.get('productions', [])
-            else:
-                data = request.POST.getlist('productions[]')
-        except Exception as e:
-            return JsonResponse({'error': f'Invalid input format: {str(e)}'})
+def compute_first(symbol): 
 
-        # Parse productions
-        for prod in data:
-            prod = prod.replace(" ", "")
-            if prod.lower() in ['end', '']:
-                continue
+    global production_list, nt_list, t_list
 
-            production_list.append(prod)
-            head, body = prod.split('->')
 
-            if head not in nt_list:
-                nt_list[head] = NonTerminal(head)
+    if symbol in t_list:
+        return set(symbol)
 
-            for symbol in body:
-                if not symbol.isupper():
-                    if symbol not in t_list:
-                        t_list[symbol] = Terminal(symbol)
-                else:
-                    if symbol not in nt_list:
-                        nt_list[symbol] = NonTerminal(symbol)
+    for prod in production_list:
+        head, body=prod.split('->')
+        
+        if head!=symbol: continue
 
-        # FIRST
-        def compute_first(symbol):
-            if symbol in t_list:
-                return set(symbol)
 
-            for prod in production_list:
-                head, body = prod.split('->')
-                if head != symbol:
-                    continue
+        if body=='':
+            nt_list[symbol].add_first(chr(1013))
+            continue
 
-                if body == '':
-                    nt_list[symbol].add_first(chr(1013))
-                    continue
+        
 
-                for i, Y in enumerate(body):
-                    if body[i] == symbol:
-                        continue
-                    t = compute_first(Y)
-                    nt_list[symbol].add_first(t - set(chr(1013)))
-                    if chr(1013) not in t:
-                        break
-                    if i == len(body) - 1:
-                        nt_list[symbol].add_first(chr(1013))
-            return nt_list[symbol].first
+        for i, Y in enumerate(body):
 
-        def get_first(symbol):
-            return compute_first(symbol)
+            if body[i]==symbol: continue
+            t=compute_first(Y)
+            nt_list[symbol].add_first(t-set(chr(1013)))
+            if chr(1013) not in t:
+                break 
 
-        # FOLLOW
-        def compute_follow(symbol):
-            if symbol == list(nt_list.keys())[0]:
-                nt_list[symbol].add_follow('$')
+            if i==len(body)-1: 
+                nt_list[symbol].add_first(chr(1013))
 
-            for prod in production_list:
-                head, body = prod.split('->')
-                for i, B in enumerate(body):
-                    if B != symbol:
-                        continue
-                    if i != len(body) - 1:
-                        nt_list[symbol].add_follow(get_first(body[i + 1]) - set(chr(1013)))
-                    if i == len(body) - 1 or chr(1013) in get_first(body[i + 1]):
-                        if B != head:
-                            nt_list[symbol].add_follow(get_follow(head))
+    return nt_list[symbol].first
 
-        def get_follow(symbol):
-            if symbol in t_list:
-                return None
-            return nt_list[symbol].follow
 
-        # Execute
-        for nt in nt_list:
-            compute_first(nt)
-        for nt in nt_list:
-            compute_follow(nt)
 
-        # Result formatting
-        result = {
-            nt: {
-                'FIRST': sorted(list(nt_list[nt].first)),
-                'FOLLOW': sorted(list(nt_list[nt].follow))
-            } for nt in nt_list
-        }
+def get_first(symbol): 
 
-        return JsonResponse({'result': result}, status=200)
+    return compute_first(symbol)
 
-    return JsonResponse({'error': 'Only POST method allowed'}, status=405)
+
+def compute_follow(symbol):
+
+    global production_list, nt_list, t_list
+
+    if symbol == list(nt_list.keys())[0]: 
+        nt_list[symbol].add_follow('$')
+
+    for prod in production_list:    
+        head, body=prod.split('->')
+
+        for i, B in enumerate(body):        
+            if B != symbol: continue
+
+
+            if i != len(body)-1:
+                nt_list[symbol].add_follow(get_first(body[i+1]) - set(chr(1013)))
+
+            if i == len(body)-1 or chr(1013) in get_first(body[i+1]) and B != head: 
+                nt_list[symbol].add_follow(get_follow(head))
+
+
+def get_follow(symbol):
+
+    global nt_list, t_list
+
+    if symbol in t_list.keys():
+        return None
+    
+    return nt_list[symbol].follow
+
+
+def main(pl=None):
+
+    print("ENTER THE FILENAME OF THE PRODUCTION RULES:")
+
+    filename = input()
+    F = open(filename,"r")
+
+    global production_list, t_list, nt_list
+    ctr=1
+
+
+    if pl==None:
+
+        for line in F:
+            line = line.replace(' ','')
+            line = line.replace('\n','')
+            production_list.append(line)
+            if production_list[-1].lower() in ['end', '']: 
+                del production_list[-1]
+                break
+
+            head, body=production_list[ctr-1].split('->')
+
+            if head not in nt_list.keys():
+                nt_list[head]=NonTerminal(head)
+
+            for i in body:
+                if not 65<=ord(i)<=90:
+                    if i not in t_list.keys(): t_list[i]=Terminal(i)
+                elif  i not in nt_list.keys(): nt_list[i]=NonTerminal(i)
+                
+            ctr+=1
+
+                
+    return pl
+
+if __name__=='__main__':
+    
+    main()
